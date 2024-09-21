29e84d52f7892811813ae0e420843216e3c5c3ef
castaneday <yazmincastaneda23@yahoo.com>
2024-09-21 12:04:58 -0500

    Added API integration & gave the AI instructions

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
-function generateWorkout(event) {
-  event.preventDefault();
-  let workoutElement = document.querySelector("#workout-split");
-
+function displayWorkout(response) {
+  console.log("workout generated");
   new Typewriter("#workout-split", {
-    strings: "Bent-over rows with barbell",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     curser: "",
   });
+}
+
+function generateWorkout(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "ce89a5b084t4180e11b02bocfa29d243";
+  let context =
+    "You are a fitness trainer expert and love to help people with their fitness goals. Your mission is to generate 5 to 6 exercises with reps based on the muscle group wanting to work on. Please provide answer in basic HTML. Make sure to follow the user instructions.";
+  let prompt = `User instructions: Generate a workout split based on ${instructionsInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating workout");
+  console.log(`prompt: is ${prompt}`);
+  console.log(`context: ${context}`);
+
+  axios.get(apiUrl).then(displayWorkout);
 }
 
 let workoutFormElement = document.querySelector("#workout-generator-form");
